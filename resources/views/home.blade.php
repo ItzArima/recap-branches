29d3b09ae8c4307e5c55f94e1b22f946238ef750
ItzArima <skaypaprilia@gmail.com>
2022-01-12 16:37:40 +0100

    changed home content

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,9 +5,10 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="main.css">
 </head>
 <body>
-    <h1>we we</h1>
+    <h1>io te posso canta una canzone</h1>
+    <p>sei sotto il cielo sbagliato</p>
 </body>
 </html>
